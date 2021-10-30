25d77f55278f456ab5a85cd000920a3e5877b47b
eugeneb-1984 <eugeneb1984@gmail.com>
2021-10-30 12:15:39 +0300

    Added task 2 and 3. Task 3 does NOT pass tests

---
+++ Homework1/Task2/SumCount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public static int StrangeSum(int[] inputArray)  
+            {
+                int sum = 0; //O(1)
+                for (int i = 0; i < inputArray.Length; i++) //O(N+1)
+                {
+                    for (int j = 0; j < inputArray.Length; j++) //O(N^2+1)
+                    {
+                        for (int k = 0; k < inputArray.Length; k++) //O(N^3+1)
+                        {
+                            int y = 0; //O(2N^3+1)
+
+                            if (j != 0) //O(3N^3+1)
+                            {
+                                y = k / j;
+                            }
+
+                            sum += inputArray[i] + i + k + j + y; //O(3N^3+2)
+                        }
+                    }
+                }
+
+                return sum; //O(3N^3+3)
+            }
+
+            //Итого: O(N^3).
+        }
+    }
+}
---
+++ Homework1/Task3/FibonacciRec/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciRec
+{
+    class Program
+    {
+         static int countFibonacci(int x, int y, int count, int length)
+        {
+            count++;
+            int fibPrev2 = y;
+            int fibTargetValue = x + y;
+            Console.WriteLine($"next={fibTargetValue}, count={count}");
+            if (count < length)
+            {
+                countFibonacci(fibPrev2, fibTargetValue, count, length);
+            }
+            int result = fibTargetValue;
+            return result;
+        }
+        public class FibTestCase
+        {
+            public int FibFirst = 0;
+            public int FibSecond = 1;
+            public int Count = 0;
+            public int FibTargetSeqNumber { set; get; }
+            public int FibTargetExpectedValue { get; set; }
+            public Exception ExpectedEx { get; set; }
+        }
+
+        static void validateCountFibonacci(FibTestCase fibTestCase)
+        {
+            try
+            {
+                int fibTargetActualValue = countFibonacci(fibTestCase.FibFirst, fibTestCase.FibSecond, fibTestCase.Count, fibTestCase.FibTargetSeqNumber);
+                Console.WriteLine(fibTargetActualValue == fibTestCase.FibTargetExpectedValue ? "Test PASSED" : $"Test FAILED. \n Expected: {fibTestCase.FibTargetExpectedValue} \n Actual: {fibTargetActualValue}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(fibTestCase.ExpectedEx == ex ? "PASSED: valid exception" : $"FAILED: Invalid exception. \n Expected: {fibTestCase.ExpectedEx.Message} \n Actual: {ex.Message}");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var Case1 = new FibTestCase()
+            {
+                FibTargetSeqNumber = 20,
+                FibTargetExpectedValue = 6765,
+                ExpectedEx = null
+            };
+
+            validateCountFibonacci(Case1);
+            Console.ReadKey();
+        }
+    }
+}
