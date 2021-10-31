a862c14d4cdd1adfc1f10175d9d2dadd5e436884
eugeneb-1984 <eugeneb1984@gmail.com>
2021-10-31 13:10:30 +0300

    Fixed countFibonacci in FibonacciRec project. Task 3 now PASSES tests.
    Still can't figure out why recursion goes backwards once completing the intended last step.

--- Homework1/Task3/FibonacciRec/Program.cs
+++ Homework1/Task3/FibonacciRec/Program.cs
@@ -8,26 +8,30 @@ namespace FibonacciRec
 {
     class Program
     {
-         static int countFibonacci(int x, int y, int count, int length)
+         public static void countFibonacci(int x, int y, int count, int length, FibTestCase fibTest)
         {
             count++;
-            int fibPrev2 = y;
-            int fibTargetValue = x + y;
-            Console.WriteLine($"next={fibTargetValue}, count={count}");
+            int fibPrev = y;
+            int fibLast = x + y;
+            Console.WriteLine($"next={fibLast}, Seq Number={count}");
             if (count < length)
             {
-                countFibonacci(fibPrev2, fibTargetValue, count, length);
+                countFibonacci(fibPrev, fibLast, count, length, fibTest);
+            }  
+            else
+            {
+                fibTest.FibTargetActualValue = fibLast;
             }
-            int result = fibTargetValue;
-            return result;
         }
         public class FibTestCase
         {
             public int FibFirst = 0;
             public int FibSecond = 1;
-            public int Count = 0;
+            public int Count = 2;
             public int FibTargetSeqNumber { set; get; }
             public int FibTargetExpectedValue { get; set; }
+
+            public int FibTargetActualValue { get; set; }
             public Exception ExpectedEx { get; set; }
         }
 
@@ -35,8 +39,8 @@ namespace FibonacciRec
         {
             try
             {
-                int fibTargetActualValue = countFibonacci(fibTestCase.FibFirst, fibTestCase.FibSecond, fibTestCase.Count, fibTestCase.FibTargetSeqNumber);
-                Console.WriteLine(fibTargetActualValue == fibTestCase.FibTargetExpectedValue ? "Test PASSED" : $"Test FAILED. \n Expected: {fibTestCase.FibTargetExpectedValue} \n Actual: {fibTargetActualValue}");
+                countFibonacci(fibTestCase.FibFirst, fibTestCase.FibSecond, fibTestCase.Count, fibTestCase.FibTargetSeqNumber, fibTestCase);
+                Console.WriteLine(fibTestCase.FibTargetActualValue == fibTestCase.FibTargetExpectedValue ? "Test PASSED" : $"Test FAILED. \n Expected: {fibTestCase.FibTargetExpectedValue} \n Actual: {fibTestCase.FibTargetActualValue}");
             }
             catch (Exception ex)
             {
@@ -49,11 +53,27 @@ namespace FibonacciRec
             var Case1 = new FibTestCase()
             {
                 FibTargetSeqNumber = 20,
-                FibTargetExpectedValue = 6765,
+                FibTargetExpectedValue = 4181,
+                ExpectedEx = null
+            };
+
+            var Case2 = new FibTestCase()
+            {
+                FibTargetSeqNumber = 40,
+                FibTargetExpectedValue = 63245986,
+                ExpectedEx = null
+            };
+
+            var Case3 = new FibTestCase()
+            {
+                FibTargetSeqNumber = 30,
+                FibTargetExpectedValue = 514229,
                 ExpectedEx = null
             };
 
             validateCountFibonacci(Case1);
+            validateCountFibonacci(Case2);
+            validateCountFibonacci(Case3);
             Console.ReadKey();
         }
     }
